6bd3bd8c7e8d4d83257dd880553ea7ca9a220fca
mangareira <ghabryellsantos81@gmail.com>
2024-04-18 15:21:38 -0300

    modifyer-treatment

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,17 +5,20 @@ import { IPayload } from "../interface/IPayload";
 
 export async function authMiddleware(req: Request, res: Response, next: NextFunction,) {
     const authHeader:any = req.headers.authorization
-    const [, token] = authHeader.split(' ')  
-    
+    const [, token] = authHeader.split(' ')          
     try {
         const secretKey = process.env.TOKEN_SECRET_KEY
         if (!secretKey) {
             throw new HttpException(400,'No secret key')
         }
         const isVaule = verify(token, secretKey ) as IPayload        
-        return next()
     } catch (error: any) {
-        //return res.status(401).json(error)
+        if(error.message == 'invalid token') {
+            return res.status(401).json({
+                code: 'token.unauthorized',
+                message: 'Unauthorized'
+            })
+        }
         if(error.message == 'invalid signature') {
             return res.status(401).json({
                 code: 'token.unauthorized',
@@ -28,6 +31,12 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
                 message: 'token expired'
             })
         }
+        if(error.message == 'jwt malformed') {
+            return res.status(401).json({
+                code: 'token',
+                message: 'token is missing'
+            })
+        }
     }
-
+    next()
 }
