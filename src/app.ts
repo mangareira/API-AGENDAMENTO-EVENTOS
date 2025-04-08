30484d0849cbb27324763809e76d1d3e256df7c9
mangareira <ghabryellsantos81@gmail.com>
2025-04-08 09:55:29 -0300

    make-a-mistake

--- src/app.ts
+++ src/app.ts
@@ -24,14 +24,14 @@ export class App{
     private middlewareInitializer() {
         this.app.use(express.json())
         this.app.use(cors({
-            origin: "http://localhost:3000",
+            origin: "https://eventos.unicentroma.edu.br",
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
             allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true
         }));
         
         this.app.options('*', (req, res) => {
-            res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+            res.header("Access-Control-Allow-Origin", "https://eventos.unicentroma.edu.br");
             res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
             res.header("Access-Control-Allow-Credentials", "true");
