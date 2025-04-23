d2f866f34cca7c286f32839d4caa20ad81352eac
mangareira <ghabryellsantos81@gmail.com>
2025-04-22 21:37:14 -0300

    add-more-rules-to-list

--- src/useCases/Event.usecase.ts
+++ src/useCases/Event.usecase.ts
@@ -632,7 +632,7 @@ export class EventUseCase {
                     const participant = await userAccount.findUserById(participantId);
                     const paymentConfirm = await user.findUser(participantId);
                     
-                    if (paymentConfirm?.payment.status === "Pago" || "gratis") {
+                    if (paymentConfirm?.payment.status === "Pago" || paymentConfirm?.payment.status === "gratis") {
                         return { name: participant?.name };
                     }
                     return null;
